5209a9e8ec447f5778dd1fdcb57cbd04b8722388
Kelly de Vries <kdv@me.com>
2019-03-14 13:52:49 -0700

    add switch case to update time to non-military

--- src/components/FiveDayForecast.js
+++ src/components/FiveDayForecast.js
@@ -5,10 +5,50 @@ const FiveDayForecast = props => {
   let weatherArray = Object.entries(weatherObject)
 
   let tempArray = weatherArray.map(item => {
-    var newest = Object.entries(item)
-    console.log("newest: ", newest[1][1])
-    var single = newest[1][1]
-    return <li key={single.dt}>{single.main.temp}</li>
+    let newest = Object.entries(item)
+    let single = newest[1][1]
+    let thedate = new Date(single.dt_txt)
+
+    // 1. get the date to check if they're the same
+    let date = thedate.getDate()
+
+    // 2. convert time to non-military
+    let time = thedate.getHours()
+    let forecastTime
+    switch (time) {
+      case 0:
+        forecastTime = "12am"
+        break
+      case 3:
+        forecastTime = "3am"
+        break
+      case 6:
+        forecastTime = "6am"
+        break
+      case 9:
+        forecastTime = "9am"
+        break
+      case 12:
+        forecastTime = "noon"
+        break
+      case 15:
+        forecastTime = "3pm"
+        break
+      case 18:
+        forecastTime = "6pm"
+        break
+      case 21:
+        forecastTime = "9pm"
+        break
+      default:
+        forecastTime = "who knows when!"
+    }
+
+    return (
+      <li key={single.dt}>
+        date: {date}: {forecastTime} - {single.main.temp}
+      </li>
+    )
   })
 
   return (
