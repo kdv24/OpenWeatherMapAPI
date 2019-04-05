4329936dfb40acf6ef22171dcd81b07312798825
Kelly de Vries <kdv@me.com>
2019-04-05 12:19:17 -0700

    update to only show date once and with written out month

--- src/components/FiveDayForecast.js
+++ src/components/FiveDayForecast.js
@@ -3,18 +3,61 @@ import React from "react"
 const FiveDayForecast = props => {
   let weatherObject = props.fiveDayForecast
   let weatherArray = Object.entries(weatherObject)
+  let currentDate = new Date() // set to today - April 5
+  let today = currentDate.getDate()
+  let printedDate = today
+  let forecastDate
 
   let tempArray = weatherArray.map(item => {
     let newest = Object.entries(item)
     let single = newest[1][1]
-    let thedate = new Date(single.dt_txt)
+    let theDate = new Date(single.dt_txt)
+    let forecastMonth = theDate.getMonth() + 1
+    switch (forecastMonth) {
+      case 0:
+        forecastMonth = "January"
+        break
+      case 1:
+        forecastMonth = "February"
+        break
+      case 3:
+        forecastMonth = "March"
+        break
+      case 4:
+        forecastMonth = "April"
+        break
+      case 5:
+        forecastMonth = "May"
+        break
+      case 6:
+        forecastMonth = "June"
+        break
+      case 7:
+        forecastMonth = "July"
+        break
+      case 8:
+        forecastMonth = "August"
+        break
+      case 9:
+        forecastMonth = "September"
+        break
+      case 10:
+        forecastMonth = "October"
+        break
+      case 11:
+        forecastMonth = "November"
+        break
+      default:
+        forecastMonth = "Unknown month"
+    }
+
+    forecastDate = theDate.getDate()
 
     // 1. get the date to check if they're the same
-    let date = thedate.getDate()
 
-    // 2. convert time to non-military
-    let time = thedate.getHours()
     let forecastTime
+    // 2. convert time to non-military
+    let time = theDate.getHours()
     switch (time) {
       case 0:
         forecastTime = "12am"
@@ -43,10 +86,52 @@ const FiveDayForecast = props => {
       default:
         forecastTime = "who knows when!"
     }
+    if (forecastDate === today) {
+      printedDate = today
+      return (
+        <li style={{ listStyle: "none" }} key={single.dt}>
+          <strong>
+            Weather for {forecastMonth} {forecastDate}:
+          </strong>
+          <br />{" "}
+          <span
+            style={{ display: "flex", justifyContent: "space-around" }}
+            key={Math.random()}
+          >
+            <em>{forecastTime}:</em> {Math.round(single.main.temp)} ℉
+          </span>
+        </li>
+      )
+    } else {
+      if (forecastDate > today) {
+        if (forecastDate === printedDate) {
+          return (
+            <span
+              style={{ display: "flex", justifyContent: "space-around" }}
+              key={Math.random()}
+            >
+              <em>{forecastTime}:</em> {Math.round(single.main.temp)} ℉
+              <br />
+            </span>
+          )
+        } else {
+          printedDate = forecastDate
+        }
+      }
+    }
 
     return (
-      <li key={single.dt}>
-        date: {date}: {forecastTime} - {single.main.temp}
+      <li
+        style={{
+          listStyle: "none",
+          paddingTop: "10px",
+          marginBottom: "0px",
+        }}
+        key={single.dt}
+      >
+        <strong>
+          Weather for {forecastMonth} {forecastDate}:
+        </strong>{" "}
       </li>
     )
   })
@@ -54,7 +139,14 @@ const FiveDayForecast = props => {
   return (
     <div style={{ display: "flex" }}>
       {/* Five Day Forecast results: */}
-      <ul>{tempArray}</ul>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {tempArray}
+      </ul>
     </div>
   )
 }
